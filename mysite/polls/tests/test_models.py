5b316bc609ca646af24011727bd68136f4cbf5c1
Vladimir Baranov <vbaranov@iponweb.net>
2023-08-10 19:50:28 +0300

    rework tests

--- mysite/polls/tests/test_models.py
+++ mysite/polls/tests/test_models.py
@@ -7,22 +7,18 @@ from polls.models import Question
 class TestQuestionModel():
 
     def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        assert future_question.was_published_recently() is False
+        assert (
+            future_question.was_published_recently() is False
+        ), "Question date in the future => was_published_recently() returns False"
 
     def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
-        """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
-        assert old_question.was_published_recently() is False
+        assert (
+            old_question.was_published_recently() is False
+        ), "Question date older than 1 day => was_published_recently() returns False"
 
     def test_was_published_recently_with_recent_question(self):
         """
@@ -31,4 +27,6 @@ class TestQuestionModel():
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
-        assert recent_question.was_published_recently() is True
+        assert (
+            recent_question.was_published_recently() is True
+        ), "Question older than 1 day => was_published_recently() returns True"
