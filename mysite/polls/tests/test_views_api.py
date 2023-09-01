365b8885c26e34ef4b8f85fbe7fc0b088e048e9a
Vladimir Baranov <vbaranov@iponweb.net>
2023-09-01 11:28:23 +0300

    fix linters

--- mysite/polls/tests/test_views_api.py
+++ mysite/polls/tests/test_views_api.py
@@ -1,3 +1,5 @@
+# pylint: disable=redefined-outer-name
+
 from http import HTTPStatus
 from faker import Faker
 import pytest
@@ -13,7 +15,8 @@ faker = Faker()
 
 @pytest.fixture
 def api_client():
-    user = User.objects.create_user(username='testuser123', password='testpassword', is_staff=True, is_superuser=True)
+    user = User.objects.create_user(
+        username='testuser123', password='testpassword', is_staff=True, is_superuser=True)
     api_client = APIClient()
     # api_client.login(username='testuser', password='testpassword')
     api_client.force_authenticate(user=user)
@@ -38,7 +41,9 @@ class TestQuestionListAPI():
         assert response.status_code == HTTPStatus.CREATED
         assert Question.objects.count() == 1
         assert Question.objects.get().question_text == 'New Question'
-        assert Question.objects.get().pub_date.strftime("%Y-%m-%dT%H:%M:%SZ") == '2023-08-28T12:00:00Z'
+        assert (
+            Question.objects.get().pub_date.strftime("%Y-%m-%dT%H:%M:%SZ") == '2023-08-28T12:00:00Z'
+        )
 
 
 @pytest.mark.django_db
@@ -46,25 +51,30 @@ class TestQuestionDetailAPI():
 
     def test_get_question_detail(self, api_client):
         Question.objects.all().delete()
-        question = Question.objects.create(question_text=faker.sentence(), pub_date='2023-08-28T12:00:00Z')
+        question = Question.objects.create(
+            question_text=faker.sentence(), pub_date='2023-08-28T12:00:00Z')
 
         response = api_client.get(reverse('api-question-detail', args=[question.pk]))
 
         assert response.status_code == HTTPStatus.OK
 
     def test_update_question(self, api_client):
-        question = Question.objects.create(question_text='Question1', pub_date='2023-08-28T12:00:00Z')
+        question = Question.objects.create(
+            question_text='Question1', pub_date='2023-08-28T12:00:00Z')
 
         data = {'question_text': 'UpdatedText', 'pub_date': '2022-08-28T12:00:00Z'}
-        response = api_client.put(reverse('api-question-detail', args=[question.pk]), data, format='json')
+        response = api_client.put(
+            reverse('api-question-detail', args=[question.pk]), data, format='json')
 
         assert response.status_code == HTTPStatus.OK
         updated_question = Question.objects.get(pk=question.pk)
         assert updated_question.question_text == 'UpdatedText'
 
     def test_delete_question(self, api_client):
-        question = Question.objects.create(question_text='Question1', pub_date='2023-08-28T12:00:00Z')
-        response = api_client.delete(reverse('api-question-detail', args=[question.pk]), format='json')
+        question = Question.objects.create(
+            question_text='Question1', pub_date='2023-08-28T12:00:00Z')
+        response = api_client.delete(
+            reverse('api-question-detail', args=[question.pk]), format='json')
 
         assert response.status_code == HTTPStatus.NO_CONTENT
         assert Question.objects.filter(pk=question.pk).exists() is False
